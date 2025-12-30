7c3380eb81df1c62b9b1382995852d40adec6bed
Maximiliano Correa <jmaxicorrea@gmail.com>
2025-12-30 17:00:33 -0300

    test: Create Send notification unit test case

---
+++ src/test/java/application/SendNotificationUseCaseTest.java
@@ -0,0 +1,48 @@
+package application;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import domain.Notification;
+import domain.NotificationFactory;
+import domain.NotificationType;
+
+class SendNotificationUseCaseTest {
+
+	@Mock
+	private NotificationFactory factory;
+	
+	@Mock
+	private Notification notification;
+	
+	private SendNotificationUseCase usecase;
+	
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(this);
+		usecase = new SendNotificationUseCase(factory);
+	}
+	
+	@Test
+	void shouldCreateNotificationAndSendMessage() {
+		// given
+		NotificationType type = NotificationType.EMAIL;
+		String message = "Hello Youtube";
+		
+		// when
+		when(factory.create(type)).thenReturn(notification);
+		usecase.execute(type, message);
+		
+		// then
+		verify(factory).create(type);
+		verify(notification).send(message);
+		verifyNoMoreInteractions(factory, notification);
+	}
+
+}
